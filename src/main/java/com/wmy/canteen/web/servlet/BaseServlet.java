ed84786ce62ccf44c5e7aac421bd3fc3489907cd
three-water666 <huangsanshui666@gmail.com>
2021-08-11 21:57:50 +0800

    无事发生

---
+++ src/main/java/com/wmy/canteen/web/servlet/BaseServlet.java
@@ -0,0 +1,16 @@
+package com.wmy.canteen.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class BaseServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+}
---
+++ src/main/java/com/wmy/canteen/web/servlet/UserServlet.java
@@ -0,0 +1,13 @@
+package com.wmy.canteen.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/userServlet")
+public class UserServlet extends BaseServlet {
+
+}
